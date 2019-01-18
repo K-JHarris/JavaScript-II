fdef7caa9cd8e00f8d00efc1959b28da46cce255
Kay Jay <kharris2k16@gmail.com>
2019-01-18 13:43:22 -0800

    Finished Closure section

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,62 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const sayHello = function(){
+  console.log("Hello...");
+  hellotext = "is it me you're looking for.";
+  function actualfunction(){
+    console.log(hellotext);
+  }
+  return actualfunction();
+}
+sayHello();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  function thisisahorriblenameforafunction() {
+    return num++;
+  }
+  return thisisahorriblenameforafunction;        
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// const counterFactory = () => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+
+//   population = ["denice", "eddy", "steveo", "johnny knoxville"]
+
+//   function Cull {
+    
+//   }
+// };
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let oprah = {};
+  let value = 0;
+  oprah.increment = function() {
+    return value++;
+  }
+  oprah.decrement = function() {
+    return value--;
+  }
+  return oprah;
+}
+
+const test = counterFactory();
+console.log(test.increment());
+console.log(test.increment());
+console.log(test.increment());
+console.log(test.increment());
